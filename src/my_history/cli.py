6e4d722edd9023420b9352c301550f59f134249a
hun0219 <gsw98000@gmail.com>
2025-05-12 14:49:25 +0900

    cli edit

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -3,8 +3,18 @@ import sys
 
 a = sys.argv[1]
 
-def cnt():
+def cnt(a):
     df = pd.read_parquet("~/tmp/history.parquet")
     fdf = df[df['cmd'].str.contains(a)]
     cnt = fdf['cnt'].sum()
-    print(cnt)
+    return(cnt)
+
+def read_parquet(path="~/tmp/history.parquet"):
+    df = pd.read_parquet("~/tmp/history.parquet")
+    return df
+
+def query():
+    i = cnt(a)
+    print(f'질의:{a}에 대한 결과는 {i}입니다')
+    print("질의:%s에 대한 결과는 %s입니다" % (a,i))
+    print("질의:%10s에 대한 결과는 %d입니다" % (a,i))
